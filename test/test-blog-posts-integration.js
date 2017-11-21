3d25b5250c1f22feb2271ab4fb2a1cd7eac2caea
Carly Sierra <carlylwomack@gmail.com>
2017-11-20 23:50:16 -0600

    added tests

--- test/test-blog-posts-integration.js
+++ test/test-blog-posts-integration.js
@@ -64,6 +64,118 @@ describe('Blog Posts API Resource', function() {
                 });
         });
 
-        
+        it('should return posts with correct fields', function() {
+            let resPost;
+            return chai.request(app)
+                .get('/posts')
+                .then(function(res) {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    res.body.should.have.length.of.at.least(1);
+
+                    res.body.forEach(function(post) {
+                        post.should.be.a('object');
+                        post.should.include.keys('id', 'title', 'content', 'author', 'created');
+                    });
+                    resPost = res.body[0];
+                    return BlogPost.findById(resPost.id);
+                })
+                .then(post => {
+                    resPost.title.should.equal(post.title);
+                    resPost.content.should.equal(post.content);
+                    resPost.author.should.equal(post.authorName);
+                });
+        });
     });
-})
+
+    describe('Post endpoint', function() {
+        it('should add a new blog post', function() {
+            const newPost = {
+                title: faker.lorem.sentence(),
+                author: {
+                  firstName: faker.name.firstName(),
+                  lastName: faker.name.lastName(),
+                },
+                content: faker.lorem.text()
+            };
+
+            return chai.request(app)
+            .post('/posts')
+            .send(newPost)
+            .then(function(res) {
+              res.should.have.status(201);
+              res.should.be.json;
+              res.body.should.be.a('object');
+              res.body.should.include.keys(
+                'id', 'title', 'content', 'author', 'created');
+              res.body.title.should.equal(newPost.title);
+              // cause Mongo should have created id on insertion
+              res.body.id.should.not.be.null;
+              res.body.author.should.equal(
+                `${newPost.author.firstName} ${newPost.author.lastName}`);
+              res.body.content.should.equal(newPost.content);
+              return BlogPost.findById(res.body.id);
+            })
+            .then(function(post) {
+              post.title.should.equal(newPost.title);
+              post.content.should.equal(newPost.content);
+              post.author.firstName.should.equal(newPost.author.firstName);
+              post.author.lastName.should.equal(newPost.author.lastName);
+            });
+        });
+    });
+
+    describe('PUT endpoint', function() {
+        
+        it('should update fields sent over', function() {
+            const updateData = {
+            title: 'How to Cook a Turkey',
+            content: 'Put it in the oven and wait.',
+            author: {
+                firstName: 'Gobble',
+                lastName: 'Gobble'
+            }
+            };
+
+            return BlogPost
+            .findOne()
+            .then(post => {
+                updateData.id = post.id;
+
+                return chai.request(app)
+                .put(`/posts/${post.id}`)
+                .send(updateData);
+            })
+            .then(res => {
+                res.should.have.status(204);
+                return BlogPost.findById(updateData.id);
+            })
+            .then(post => {
+                post.title.should.equal(updateData.title);
+                post.content.should.equal(updateData.content);
+                post.author.firstName.should.equal(updateData.author.firstName);
+                post.author.lastName.should.equal(updateData.author.lastName);
+            });
+        });
+    });
+        
+    describe('DELETE endpoint', function() {
+        it('should delete a post by id', function() {
+            let post;
+            return BlogPost
+            .findOne()
+            .then(_post => {
+                post = _post;
+                return chai.request(app).delete(`/posts/${post.id}`);
+            })
+            .then(res => {
+                res.should.have.status(204);
+                return BlogPost.findById(post.id);
+            })
+            .then(_post => {
+                should.not.exist(_post);
+            });
+        });
+    });
+});
